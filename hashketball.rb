12436250976bdc03f2b48b675f04f14f9b15537e
Fillan Lee <fillanlee@aol.com>
2019-02-17 15:34:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,31 @@
 # Write your code here!
 
-
+game_hash{
+  home{:team_name =>{},
+      :colors => {},
+       :players =>{:number =>{},
+                    :shoes =>{},
+                    :points => {},
+                    :rebounds => {},
+                    :assits => {},
+                    :steals => {},
+                    :blocks => {},
+                    :slam_dunks => {}
+       },
+   away{:team_name =>{},
+      :colors => {},
+       :players =>{:number =>{},
+                    :shoes =>{},
+                    :points => {},
+                    :rebounds => {},
+                    :assits => {},
+                    :steals => {},
+                    :blocks => {},
+                    :slam_dunks => {}
+       }
+       
+       
+}
 
 
 
